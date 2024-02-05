70116310aac80424b6b626aa0ccf73992dc2d161
大聪明 <601670314@qq.com>
2024-02-05 01:56:06 -0800

    Update rollup.config.mjs

--- scripts/rollup.config.mjs
+++ scripts/rollup.config.mjs
@@ -1,5 +1,4 @@
 import typescript from "rollup-plugin-typescript2";
-// import resolve from "@rollup/plugin-node-resolve";
 
 export default {
 	input: "src/index.ts",
@@ -15,5 +14,10 @@ export default {
 		typescript({
 			tsconfig: "./tsconfig.json",
 		}),
+	],
+	external: [
+		"@valeera/idgenerator",
+		"@valeera/eventfire",
+		"@valeera/tree",
 	],
 };
