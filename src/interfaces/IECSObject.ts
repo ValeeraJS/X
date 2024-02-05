d826d504609be3ca304c304e3ced6b26c791eff1
大聪明 <601670314@qq.com>
2024-02-05 02:01:19 -0800

    Update IECSObject.ts

--- src/interfaces/IECSObject.ts
+++ src/interfaces/IECSObject.ts
@@ -1,8 +1,10 @@
+import { IEventFirer } from "@valeera/eventfire";
 import { IManager } from "./IManager";
 import { ISerializable } from "./ISerializable";
 
-export interface IECSObject<T> extends ISerializable {
+export interface IECSObject<T> extends IEventFirer, ISerializable {
+	readonly id: number;
 	disabled: boolean;
 	name: string;
-	usedBy: IManager<T>[];
+	usedBy: IManager<T, IECSObject<any>>[];
 }
