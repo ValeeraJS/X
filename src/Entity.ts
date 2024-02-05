d18efd28c87a5ccc7d6e087392b94c1e24de7fdc
大聪明 <601670314@qq.com>
2024-02-05 01:57:50 -0800

    Update Entity.ts

--- src/Entity.ts
+++ src/Entity.ts
@@ -1,151 +1,163 @@
 import { ComponentManager } from "./ComponentManager";
-import { mixin } from "@valeera/eventfire";
-import { IComponent } from "./interfaces/IComponent";
-import { ComponentConstructor, IComponentManager } from "./interfaces/IComponentManager";
+import { IEventFirer, mixin } from "@valeera/eventfire";
 import { IdGeneratorInstance } from "./Global";
-import { IEntity } from "./interfaces/IEntity";
-import { IEntityManager } from "./interfaces/IEntityManager";
-import { IWorld } from "./interfaces/IWorld";
 import { TreeNode } from "@valeera/tree";
+import { IEntitySerializedJson } from "./interfaces/ISerializable";
+import { EntityManager } from "./EntityManager";
+import { Component, ComponentConstructor } from "./Component";
+import { World } from "./World";
 
-let arr: any[];
-
-export class Entity extends mixin(TreeNode) implements IEntity {
+export class Entity extends mixin(TreeNode) implements IEventFirer {
 	public readonly id: number = IdGeneratorInstance.next();
 	public readonly isEntity = true;
-	public componentManager: IComponentManager | null = null;
+	public readonly componentManager: ComponentManager = new ComponentManager(this);
 	public disabled = false;
 	public name = "";
-	public usedBy: IEntityManager[] = [];
+	public usedBy: EntityManager[] = [];
 
-	public constructor(componentManager: IComponentManager = new ComponentManager(), name = "Untitled Entity") {
+	public constructor(name = "Untitled Entity") {
 		super();
 		this.name = name;
-		this.registerComponentManager(componentManager);
 	}
 
-	public addComponent(component: IComponent<any>): this {
-		if (this.componentManager) {
-			this.componentManager.add(component);
-		} else {
-			throw new Error("Current entity hasn't registered a component manager yet.");
+	public add(componentOrChild: Component<any> | Entity): this {
+		if (componentOrChild instanceof Entity) {
+			return this.addChild(componentOrChild);
 		}
+
+		return this.addComponent(componentOrChild);
+	}
+
+	public addComponent(component: Component<any>): this {
+		this.componentManager.add(component);
 
 		return this;
 	}
 
-	public addChild(entity: IEntity): this {
+	public addChild(entity: Entity): this {
 		super.addChild(entity);
 
-		if (this.usedBy) {
-			for (const manager of this.usedBy) {
-				manager.add(entity);
-			}
+		for (const manager of this.usedBy) {
+			manager.add(entity);
 		}
 
 		return this;
 	}
 
-	public addTo(manager: IEntityManager): this {
+	public addTo(worldOrManager: EntityManager | World | Entity): this {
+		if (worldOrManager instanceof World) {
+			return this.addToWorld(worldOrManager);
+		}
+
+		if (worldOrManager instanceof Entity) {
+			worldOrManager.addChild(this);
+
+			return this;
+		}
+
+		return this.addToManager(worldOrManager);
+	}
+
+	public addToWorld(world: World): this {
+		world.entityManager.add(this);
+
+		return this;
+	}
+
+	public addToManager(manager: EntityManager): this {
 		manager.add(this);
 
 		return this;
 	}
 
-	public addToWorld(world: IWorld): this {
-		if (world.entityManager) {
-			world.entityManager.add(this);
-		}
-
-		return this;
-	}
-
-	public clone(cloneComponenT?: Boolean) {
-		const entity = new Entity(new ComponentManager(), this.name);
-		if (cloneComponenT) {
-			this.componentManager?.elements.forEach((component) => {
+	public clone(cloneComponents?: boolean, includeChildren?: boolean) {
+		const entity = new Entity(this.name);
+		if (cloneComponents) {
+			this.componentManager.elements.forEach((component) => {
 				entity.addComponent(component.clone());
 			});
 		} else {
-			this.componentManager?.elements.forEach((component) => {
+			this.componentManager.elements.forEach((component) => {
 				entity.addComponent(component);
 			});
+		}
+		if (includeChildren) {
+			for (let i = 0, len = this.children.length; i < len; i++) {
+				entity.addChild(this.children[i].clone());
+			}
 		}
 		return entity;
 	}
 
-	public destroy(): void {
+	public destroy(): this {
 		for (const manager of this.usedBy) {
 			manager.remove(this);
 		}
-		this.unregisterComponentManager();
-	}
-
-	public getComponent(nameOrId: string | number | ComponentConstructor): IComponent<any> | null {
-		return this.componentManager?.get(nameOrId) || null;
-	}
-
-	public getComponentsByTagLabel(label: string): IComponent<any>[] {
-		return this.componentManager?.getComponentsByTagLabel(label) || [];
-	}
-
-	public getComponentByTagLabel(label: string): IComponent<any> | null {
-		return this.componentManager?.getComponentByTagLabel(label) || null;
-	}
-
-	public getComponentsByClass(clazz: ComponentConstructor): IComponent<any>[] {
-		return this.componentManager?.getComponentsByClass(clazz) || [];
-	}
-
-	public getComponentByClass(clazz: ComponentConstructor): IComponent<any> | null {
-		return this.componentManager?.getComponentByClass(clazz) || null;
-	}
-
-	public hasComponent(component: IComponent<any> | string | number): boolean {
-		return this.componentManager?.has(component) || false;
-	}
-
-	public registerComponentManager(manager: IComponentManager): this {
-		this.unregisterComponentManager();
-		this.componentManager = manager;
-		if (!this.componentManager.usedBy.includes(this)) {
-			this.componentManager.usedBy.push(this);
-		}
+		this.componentManager.elements.forEach((c) => {
+			c.destroy();
+		});
+		this.componentManager.clear();
 
 		return this;
 	}
 
-	public removeChild(entity: IEntity): this {
+	public getComponent<T>(nameOrId: string | number | ComponentConstructor<T>): Component<T> | null {
+		return this.componentManager.get(nameOrId);
+	}
+
+	public getComponentsByTagLabel(label: string): Component<any>[] {
+		return this.componentManager.getComponentsByTagLabel(label);
+	}
+
+	public getComponentByTagLabel(label: string): Component<any> | null {
+		return this.componentManager.getComponentByTagLabel(label);
+	}
+
+	public getComponentsByClass<T>(clazz: ComponentConstructor<T>): Component<T>[] {
+		return this.componentManager.getComponentsByClass(clazz);
+	}
+
+	public hasComponent(component: Component<any> | string | number | ComponentConstructor<any>): boolean {
+		return this.componentManager.has(component);
+	}
+
+	public remove(entityOrComponent: Entity | Component<any> | ComponentConstructor<any>) {
+		if (entityOrComponent instanceof Entity) {
+			return this.removeChild(entityOrComponent);
+		}
+
+		return this.removeComponent(entityOrComponent);
+	}
+
+	public removeChild(entity: Entity): this {
 		super.removeChild(entity);
 
-		if (this.usedBy) {
-			for (const manager of this.usedBy) {
-				manager.remove(entity);
-			}
+		for (const manager of this.usedBy) {
+			manager.remove(entity);
 		}
 
 		return this;
 	}
 
-	public removeComponent(component: IComponent<any> | string): this {
-		if (this.componentManager) {
-			this.componentManager.remove(component);
-		}
+	public removeComponent(component: Component<any> | string | ComponentConstructor<any>): this {
+		this.componentManager.remove(component);
 
 		return this;
 	}
 
-	public serialize(): any {
-		return {};
-	}
+	public serialize(): IEntitySerializedJson {
+		const result: IEntitySerializedJson = {
+			id: this.id,
+			name: this.name,
+			disabled: this.disabled,
+			class: "Entity",
+			components: [],
+		};
 
-	public unregisterComponentManager(): this {
-		if (this.componentManager) {
-			arr = this.componentManager.usedBy;
-			arr.splice(arr.indexOf(this) - 1, 1);
-			this.componentManager = null;
-		}
+		this.componentManager.elements.forEach((c) => {
+			result.components.push(c.id);
+		});
 
-		return this;
+		return result;
 	}
 }
