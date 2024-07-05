ed5e9bf8d82d96ad19ee909bdaf65bb7dd57b704
大聪明 <601670314@qq.com>
2024-07-04 20:49:07 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 export * from "./System";
 export * from "./Component";
-export * from "./ComponentManager";
 export * from "./Entity";
-export * from "./Manager";
-export * from "./EntityManager";
-export * from "./SystemManager";
 export * from "./World";
+export { unsortedRemove } from "./utils/unsortedRemove";
