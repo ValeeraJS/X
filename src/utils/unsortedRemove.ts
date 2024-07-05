79ef82534341008129b34644bd18463b8dcaa517
大聪明 <601670314@qq.com>
2024-07-04 20:46:10 -0700

    Create unsortedRemove.ts

---
+++ src/utils/unsortedRemove.ts
@@ -0,0 +1,14 @@
+export const unsortedRemove = <T>(arr: T[], i: number): T | undefined => {
+	if (i >= arr.length || i < 0) {
+		return;
+	}
+
+	const last = arr.pop() as T;
+	if (i < arr.length) {
+		const tmp = arr[i];
+		arr[i] = last;
+		return tmp;
+	}
+
+	return last;
+}
