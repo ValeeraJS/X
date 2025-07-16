0b67aa94910be2331cc9f1e8fe69966cca3f1ff4
大聪明 <601670314@qq.com>
2025-07-16 17:53:36 +0800

    Update entity.ts

--- test/entity.ts
+++ test/entity.ts
@@ -1,52 +1,51 @@
 /// <reference types="mocha" />
-import { Component, World } from "../src";
+import { Component, World, Entity, XXXEntity } from "../src";
 import { expect } from "chai";
-import { Entity } from "../src/Entity";
 
 describe("entity has component", function () {
     const e1 = new Entity();
     const c = new Component(0, "Component");
-	it('entity has component', function () {
-		expect(e1.hasComponent(c)).to.equal(false);
+    it('entity has component', function () {
+        expect(e1.hasComponent(c)).to.equal(false);
         e1.add(c);
-		expect(e1.hasComponent(c)).to.equal(true);
-		expect(e1.hasComponent(Component)).to.equal(true);
-		expect(e1.hasComponent("Component")).to.equal(true);
-		expect(e1.hasComponent(c.id)).to.equal(true);
+        expect(e1.hasComponent(c)).to.equal(true);
+        expect(e1.hasComponent(Component)).to.equal(true);
+        expect(e1.hasComponent("Component")).to.equal(true);
+        expect(e1.hasComponent(c.id)).to.equal(true);
 
-		expect(e1.getComponent(Component)).to.equal(c);
-		expect(e1.getComponent("Component")).to.equal(c);
-		expect(e1.getComponent(c.id)).to.equal(c);
-		expect(e1.getComponent("a")).to.equal(null);
+        expect(e1.getComponent(Component)).to.equal(c);
+        expect(e1.getComponent("Component")).to.equal(c);
+        expect(e1.getComponent(c.id)).to.equal(c);
+        expect(e1.getComponent("a")).to.equal(null);
 
         e1.remove(c);
-		expect(e1.hasComponent(c)).to.equal(false);
+        expect(e1.hasComponent(c)).to.equal(false);
 
         e1.add(c);
         e1.remove(Component);
-		expect(e1.hasComponent(c)).to.equal(false);
+        expect(e1.hasComponent(c)).to.equal(false);
 
         e1.add(c);
-		expect(e1.hasComponent(c)).to.equal(true);
+        expect(e1.hasComponent(c)).to.equal(true);
         e1.removeComponent(c.name);
-		expect(e1.hasComponent(c)).to.equal(false);
+        expect(e1.hasComponent(c)).to.equal(false);
 
-        class BBB extends Component<any> {}
+        class BBB extends Component<any> { }
         const bbb = new BBB();
         e1.add(bbb);
-		expect(e1.hasComponent(bbb)).to.equal(true);
+        expect(e1.hasComponent(bbb)).to.equal(true);
         e1.remove(Component);
-		expect(e1.hasComponent(bbb)).to.equal(true);
+        expect(e1.hasComponent(bbb)).to.equal(true);
 
-	});
+    });
 });
 
 describe("entity clone", function () {
     const e1 = new Entity();
     const c = new Component(0);
-	it('entity add to world', function () {
+    it('entity add to world', function () {
         e1.add(c);
-		const e2 = e1.clone();
+        const e2 = e1.clone();
         expect(e2.hasComponent(c)).to.equal(true);
         const e3 = e1.clone(true);
         expect(e3.hasComponent(c)).to.equal(false);
@@ -58,20 +57,20 @@ describe("entity clone", function () {
         const d = ee.clone(true, true);
         expect(!!d.children[0]).to.equal(true);
         expect(d.children[0] === ee1).to.equal(false);
-	});
+    });
 });
 
 describe("entity destroy", function () {
     const e1 = new Entity();
     const c = new Component(0);
     const world = new World();
-	it('entity destroy', function () {
+    it('entity destroy', function () {
         world.add(e1);
         e1.addComponent(c);
-		e1.destroy();
+        e1.destroy();
         expect(e1.hasComponent(c)).to.equal(false);
         expect(world.hasEntity(e1)).to.equal(false);
-	});
+    });
 });
 
 describe("entity add component", function () {
@@ -91,7 +90,7 @@ describe("entity add component", function () {
             this.bbb = bbb;
         }
     }
-	it('entity add', function () {
+    it('entity add', function () {
         e1.addComponent(TestC, 111);
         expect(e1.hasComponent(TestC)).to.equal(true);
         const c = e1.getComponent(TestC);
@@ -102,7 +101,7 @@ describe("entity add component", function () {
 
         e1.addChild(E2, 111, 'aaa');
         expect(e1.children[1] instanceof E2).to.equal(true);
-	});
+    });
 });
 
 describe("entity tags", function () {
@@ -132,21 +131,28 @@ describe("entity tags", function () {
     Entity.setTag('tagB', tagB);
     Entity.setTag('tagC', ['tagA', 'tagB', TestC]);
 
-	it('entity tag', function () {
-        expect(e1.fitTag("aaa")).to.equal(false);
-        expect(e1.fitTag("tagA")).to.equal(true);
-        expect(e1.fitTag("tagB")).to.equal(false);
+    it('entity tag', function () {
+        expect(e1.isFitTag("aaa")).to.equal(false);
+        expect(e1.isFitTag("tagA")).to.equal(true);
+        expect(e1.isFitTag("tagB")).to.equal(false);
 
         e1.addComponent(new TestA(1));
-        expect(e1.fitTag("tagA")).to.equal(true);
-        expect(e1.fitTag("tagB")).to.equal(false);
+        expect(e1.isFitTag("tagA")).to.equal(true);
+        expect(e1.isFitTag("tagB")).to.equal(false);
         e1.addComponent(new TestB(1));
-        expect(e1.fitTag("tagB")).to.equal(true);
-        expect(e1.fitTag("tagC")).to.equal(false);
+        expect(e1.isFitTag("tagB")).to.equal(true);
+        expect(e1.isFitTag("tagC")).to.equal(false);
         e1.addComponent(new TestC(1));
-        expect(e1.fitTag("tagC")).to.equal(true);
+        expect(e1.isFitTag("tagC")).to.equal(true);
 
         Entity.removeTag("tagC");
-        expect(e1.fitTag("tagC")).to.equal(false);
-	});
+        expect(e1.isFitTag("tagC")).to.equal(false);
+
+        Entity.setTag('tagC', ['tagA', 'tagB', TestC]);
+        e1.components.clear();
+        e1.fitTag("tagC");
+        e1.fitTag("tagX");
+        expect(e1.isFitTag("tagC")).to.equal(true);
+        expect(e1.isFitTag("tagX")).to.equal(false);
+    });
 });
